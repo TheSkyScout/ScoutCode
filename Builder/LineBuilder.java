52e739ec2eb9948974cd01fbeeeda95fc902dbe6
TheSkyScout <84861085+TheSkyScout@users.noreply.github.com>
2021-05-28 21:57:38 +0200

    Create LineBuilder.java

---
+++ Builder/LineBuilder.java
@@ -0,0 +1,32 @@
+
+import org.bukkit.Material;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class LineBuilder {
+
+    private Inventory inv;
+    private Material mat;
+    private String name;
+
+    public LineBuilder(Inventory inv) {
+        this.inv = inv;
+    }
+    public LineBuilder add(Material mat, String name) {
+        this.mat = mat;
+        this.name = name;
+        return this;
+    }
+
+    public LineBuilder build(int begin, int end) {
+        for(int i=begin;i<end;i++) {
+            ItemStack is = new ItemStack(mat);
+            ItemMeta im = is.getItemMeta();
+            im.setDisplayName(name);
+            is.setItemMeta(im);
+            inv.setItem(i, is);
+        }
+        return this;
+    }
+}
